1212bc8e15e73e0dd62a275a573031c33ef40605
KaitoKodama <73941704+KaitoKodama@users.noreply.github.com>
2022-04-14 13:43:11 +0900

    error handling

--- lib/utility/system.dart
+++ lib/utility/system.dart
@@ -44,36 +44,40 @@ class RequestUnitManager{
   final uid = FirebaseAuth.instance.currentUser!.uid;
 
   Future<RequestUnit> getRequestState(List<dynamic> myFriendList, List<dynamic> myRequireList, String searchID) async {
-    var targetDoc = await docRef.doc(searchID).get();
-    if(!targetDoc.exists){
-      return RequestUnit("ユーザーIDが見つかりませんでした", RequestState.IsNotExist);
-    }
-    if(targetDoc.id == uid){
-      return RequestUnit("自身をフレンドリストには追加できません", RequestState.IsSelfID);
-    }
-    final targetFriendRequireList = await targetDoc.get('friend_require');
-    for(var target in targetFriendRequireList){
-      if(target.toString() == uid){
-        return RequestUnit("申請済みです。承認をお待ちください", RequestState.IsMultipleRequire);
+    try{
+      var targetDoc = await docRef.doc(searchID).get();
+      if(!targetDoc.exists){
+        return RequestUnit("ユーザーIDが見つかりませんでした", RequestState.IsNotExist);
       }
-    }
-    for(var target in myRequireList){
-      if(target.toString() == uid){
-        return RequestUnit("申請済みです。承認をお待ちください", RequestState.IsMultipleRequire);
+      if(targetDoc.id == uid){
+        return RequestUnit("自身をフレンドリストには追加できません", RequestState.IsSelfID);
       }
-    }
-    for(var myFriend in myFriendList){
-      if(myFriend.toString() == searchID){
-        return RequestUnit("同一ユーザーが既にフレンドリストに追加されています", RequestState.IsInFriendList);
+      final targetFriendRequireList = await targetDoc.get('friend_require');
+      for(var target in targetFriendRequireList){
+        if(target.toString() == uid){
+          return RequestUnit("申請済みです。承認をお待ちください", RequestState.IsMultipleRequire);
+        }
       }
+      for(var target in myRequireList){
+        if(target.toString() == uid){
+          return RequestUnit("申請済みです。承認をお待ちください", RequestState.IsMultipleRequire);
+        }
+      }
+      for(var myFriend in myFriendList){
+        if(myFriend.toString() == searchID){
+          return RequestUnit("同一ユーザーが既にフレンドリストに追加されています", RequestState.IsInFriendList);
+        }
+      }
+      final requestTarget = MasterPartialInfo(await targetDoc.get('user_info'));
+      if(requestTarget.isLogout){
+        return RequestUnit("ログアウト中のユーザーには申請を行えません", RequestState.IsLogout);
+      }
+      String name = requestTarget.userName;
+      return RequestUnit("$nameさんへフレンド申請を行いますか？", RequestState.Accept);
     }
-    final requestTarget = MasterPartialInfo(await targetDoc.get('user_info'));
-    if(requestTarget.isLogout){
-      return RequestUnit("ログアウト中のユーザーには申請を行えません", RequestState.IsLogout);
+    catch(exe){
+      return RequestUnit("エラーを検知しました。\n入力内容に誤りがないか確認の上、再度申請を行ってください", RequestState.IsNotExist);
     }
-
-    String name = requestTarget.userName;
-    return RequestUnit("$nameさんへフレンド申請を行いますか？", RequestState.Accept);
   }
 }
 class RequestUnit{
